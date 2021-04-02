8e7dfba0d3de7104d91afa9c828eb7673a3a67b7
pbegle <peter.begle@bytecode.io>
2021-04-02 14:55:51 -0700

    Refactoring streams to be class based.

--- tap_sailthru/streams.py
+++ tap_sailthru/streams.py
@@ -1,75 +1,104 @@
-# streams: API URL endpoints to be called
 
-STREAMS = {
-    'ad_targeter_plans': {
-        'key_properties': ['plan_id'],
-        'replication_method': 'FULL_TABLE',
-    },
-    'blasts': {
-        'key_properties': ['blast_id'],
-        'replication_method': 'INCREMENTAL',
-        'replication_keys': ['modify_time'],
-        'params': {
-            'statuses': ['sent', 'sending', 'unscheduled', 'scheduled']
-        },
-        'children': {
-            'blast_recipients': {
-                'key_properties': ['user_id'],
-                'replication_method': 'FULL_TABLE',
-                'params': {
-                    'job': 'blast_query',
-                    'blast_id': '{blast_id}'
-                },
-            },
-        }
-    },
-    'blast_repeats': {
-        'key_properties': ['repeat_id'],
-        'replication_method': 'FULL_TABLE',
-    },
-    'lists': {
-        'key_properties': ['list_id'],
-        'replication_method': 'FULL_TABLE',
-        'children': {
-            'list_users': {
-                'key_properties': ['user_id'],
-                'replication_method': 'FULL_TABLE',
-                'params': {
-                    'job': 'export_list_data',
-                    'list': '{list_name}',
-                },
-                'children': {
-                    'users': {
-                        'key_properties': ['user_id'],
-                        'replication_method': 'FULL_TABLE',
-                        'params': {
-                            'id': '{user_id}',
-                            'key': 'sid',
-                        },
-                    },
-                }
-            },
-        }
-    },
-    'purchase_log': {
-        'key_properties': ['purchase_id'],
-        'replication_method': 'FULL_TABLE',
-        'replication_keys': [],
-        'params': {
-            'job': 'export_purchase_log',
-            'start_date': '{purchase_log_start_date}',
-            'end_date': '{purchase_log_end_date}',
-        },
-        'children': {
-            'purchases': {
-                'key_properties': ['item_id'],
-                'replication_method': 'INCREMENTAL',
-                'replication_keys': ['time'],
-                'params': {
-                    'purchase_id': '{purchase_id}',
-                    'purchase_key': 'sid',
-                },
-            },
-        }
-    },
-}
+class BaseStream:
+    object_type = None
+    tap_stream_id = None
+    replication_method = None
+    replication_key = None
+    key_properties = []
+    valid_replication_keys = []
+    params = {}
+
+    def __init__(self, client):
+        self.client = client
+
+
+class IncrementalStream(BaseStream):
+    replication_method = 'INCREMENTAL'
+
+    def __init__(self, client):
+        super().__init__(client)
+
+    def sync(self, state, stream_schema, stream_metadata, config, transformer):
+        pass
+
+
+class FullTableStream(BaseStream):
+    replication_method = 'FULL_TABLE'
+
+    def __init__(self, client):
+        super().__init__(client)
+
+    def sync(self, state, stream_schema, stream_metadata, config, transformer):
+        pass
+
+class AdTargeterPlans(FullTableStream):
+    tap_stream_id = 'ad_targeter_plans'
+    key_properties = ['plan_id']
+
+
+class Blasts(IncrementalStream):
+    tap_stream_id = 'blasts'
+    key_properties = ['blast_id']
+    replication_key = 'modify_time'
+    valid_replication_keys = ['modify_time']
+    params = {
+        'statuses': ['sent', 'sending', 'unscheduled', 'scheduled'],
+    }
+
+
+class BlastRecipients(FullTableStream):
+    tap_stream_id = 'blast_recipients'
+    key_properties = ['user_id']
+    params = {
+        'job': 'blast_query',
+        'blast_id': '{blast_id}',
+    }
+
+
+class BlastRepeats(FullTableStream):
+    tap_stream_id = 'blast_repeats'
+    key_properties = ['repeat_id']
+
+
+class Lists(FullTableStream):
+    tap_stream_id = 'lists'
+    key_properties = ['list_id']
+
+
+class ListUsers(FullTableStream):
+    tap_stream_id = 'list_users'
+    key_properties = ['user_id']
+    params = {
+        'job': 'export_list_data',
+        'list': '{list_name}',
+    }
+
+
+class Users(FullTableStream):
+    tap_stream_id = 'users'
+    key_properties = ['user_id']
+    params = {
+        'id': '{user_id}',
+        'key': 'sid',
+    }
+
+
+class PurchaseLog(FullTableStream):
+    tap_stream_id = 'purchase_log'
+    key_properties = ['purchase_id']
+    params = {
+        'job': 'export_purchase_log',
+        'start_date': '{purchase_log_start_date}',
+        'end_date': '{purchase_log_end_date}',
+    }
+
+
+class Purchases(IncrementalStream):
+    tap_stream_id = 'purchases'
+    key_properties = ['item_id']
+    replication_key = 'time'
+    valid_replication_keys = ['time']
+    params = {
+        'purchase_id': '{purchase_id}',
+        'purchase_key': 'sid',
+    }
