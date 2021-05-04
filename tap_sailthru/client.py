1fcb4dad602601cacd45cc2324cfd4ebdba3c299
pbegle <peter.begle@bytecode.io>
2021-05-04 15:01:31 -0700

    Added endpoints to client

--- tap_sailthru/client.py
+++ tap_sailthru/client.py
@@ -72,15 +72,97 @@ class SailthruClient:
         """
         return hashlib.md5(self.get_signature_string(params, secret)).hexdigest()
 
-    def get_lists(self) -> dict:
+    def get_lists(self, params: dict = None) -> dict:
         """
-        Queries the /list endpoint to get all the lists in Sailthru.
+        Get all the lists in Sailthru.
 
         Docs: https://getstarted.sailthru.com/developers/api/list/
 
+        :param params: Dict containig params to be passed to the API
         :return: A dict containing the API response.
         """
-        return self.get('/list', None)
+        return self.get('/list', params)
+
+    def get_ad_targeter_plans(self, params: dict = None) -> dict:
+        """
+        Get all info on Ad Targeter Plans.
+
+        Docs: https://getstarted.sailthru.com/developers/api/ad-plan/
+
+        :param params: Dict containig params to be passed to the API
+        :return: A dict containing the API response.
+        """
+        return self.get('/ad/plan', params)
+
+    def get_blasts(self, params: dict = None) -> dict:
+        """
+        Get information about campaigns (blasts).
+
+        Docs: https://getstarted.sailthru.com/developers/api/blast/
+        Endpoint does not have ability to return all blasts. Can only
+        query by blast status.
+
+        :param params: Dict containig params to be passed to the API
+        :return: A dict containing the API response.
+        """
+        if not params.get('status') and not params.get('blast_id'):
+            raise SailthruClientError('Endpoint requires either "blast_id"'
+                                      'or "status" parameter')
+
+        return self.get('/blast', params)
+
+    def get_blast_repeats(self, params: dict = None) -> dict:
+        """
+        Get all the recurring mass mail campaigns.
+
+        Docs: https://getstarted.sailthru.com/developers/api/blast_repeat/
+
+        :param params: Dict containig params to be passed to the API
+        :return: A dict containing the API response.
+        """
+        return self.get('/blast_repeat', params)
+
+    def get_user(self, params: dict = None) -> dict:
+        """
+        Get user profile data.
+
+        Docs: https://getstarted.sailthru.com/developers/api/user/
+
+        :param params: Dict containig params to be passed to the API
+        :return: A dict containing the API response.
+        """
+        if not params.get('id'):
+            raise SailthruClientError('Required "id" parameter missing')
+
+        return self.get('/user', params)
+
+    def get_job(self, params: dict = None) -> dict:
+        """
+        Get status and export URL for job.
+
+        Docs: https://getstarted.sailthru.com/developers/api/job
+
+        :param params: Dict containig params to be passed to the API
+        :return: A dict containing the API response.
+        """
+        if not params.get('job_id'):
+            raise SailthruClientError('Required "job_id" parameter missing')
+
+        return self.get('/job', params)
+
+    def post_job(self, params: dict = None) -> dict:
+        """
+        Create data export job.
+
+        Docs: https://getstarted.sailthru.com/developers/api/job
+
+        :param params: Dict containig params to be passed to the API
+        :return: A dict containing the API response.
+        """
+        if not params.get('job'):
+            raise SailthruClientError('Required "job" type parameter missing')
+
+        return self.post('/job', params)
 
     def get(self, endpoint, data):
         return self._build_request(endpoint, data, 'GET')
@@ -98,6 +180,9 @@ class SailthruClient:
                                         url=url,
                                         params=payload,
                                         headers=self.headers)
+        # TODO: handle errors and nonetype responses
+        response.raise_for_status()
+
         return response.json()
 
     def _prepare_payload(self, data):
