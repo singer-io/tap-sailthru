8322716faf8e13f277f130d405382594a8cf3941
Zach Harris <zharris@talend.com>
2021-08-27 18:22:04 +0000

    fix setup.py readme

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import find_packages, setup
+from os.path import abspath, dirname, join
 
 requirements_file = "requirements.txt"
+ROOT_DIR = abspath(dirname(__file__))
 
-with open("README.md") as f:
+
+with open(join(ROOT_DIR, "README.md"), encoding="utf-8") as f:
     readme = f.read()
 
 setup(
     name="tap-sailthru",
-    version="0.1.0",
+    version="0.1.1",
     description="Singer.io tap for the SailThru API",
     long_description=readme,
+    long_description_content_type='text/markdown',
     author="Stitch",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
